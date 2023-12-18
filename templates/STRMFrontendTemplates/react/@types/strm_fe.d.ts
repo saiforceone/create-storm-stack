68fda0dac7c57dd63f69f8ced23f91e922fb583f
sai <saiforceone@gmail.com>
2023-12-17 21:09:48 -0600

    43-update-makemodule-command - update typedefs for frontend (vue and react) - update page generation functions (index and details) - update helpers and build function to use correct file extensions for components

--- templates/STRMFrontendTemplates/react/@types/strm_fe.d.ts
+++ templates/STRMFrontendTemplates/react/@types/strm_fe.d.ts
@@ -6,13 +6,37 @@ export namespace STRMApp {
     success: boolean;
     message?: string;
     data: unknown;
-  }
-  export type APIStackComponent = {
-    component: string;
-  }
+  };
+
   export type StrmFERoute = {
     readonly path: string;
     readonly componentName: string;
     readonly componentPath: string;
-  }
+  };
+
+  export type STRMFERoute = {
+    readonly path: string;
+    readonly componentName: string;
+    readonly componentPath: string;
+  };
+
+  export type STRMController = {
+    controllerName: string;
+    endpointBase: string;
+    modelName: string;
+  };
+
+  // STRM Module - Collection of a controller, model and frontend pages
+  export type STRMModule = {
+    controller: STRMController;
+    controllerOnly: boolean;
+    pages: Array<STRMFERoute>;
+  };
+
+  // STRM Modules File - A JSON file that represents a STRM Stack module collection
+  export type STRMModulesFile = {
+    appId: string;
+    lastUpdated: string;
+    modules: Record<string, STRMModule>;
+  };
 }
